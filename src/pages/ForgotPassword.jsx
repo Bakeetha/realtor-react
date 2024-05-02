b56360863fead1227e4ce61240069b75e04a1a11
Bakeetha <76912190+Bakeetha@users.noreply.github.com>
2024-05-02 16:14:13 +0530

    Complete the forgot password page functionality - disabled the Email enumeration protection in the settings tab of Authentication in Firebase

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,11 +1,136 @@
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom';
 import OAuth from '../components/OAuth';
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { toast  } from 'react-toastify';
 export default function ForgotPassword() {
   const [email, setEmail] = useState('');
+  // const [emailMessage, setEmailMessage] = useState(false)
+  // Function to handle input change
   function onChange(event) {
     setEmail(event.target.value);
   }
+   // onSubmit is async bcoz sendPasswordResetEmail returns promise so we need to use await
+  async function onSubmit(e){
+    e.preventDefault(); //to prevent the refreshing the page
+    
+    const auth = getAuth();
+    try {
+      await sendPasswordResetEmail(auth, email);
+      toast.success("Password reset email was sent");
+    } catch (error) {
+      toast.error("Could not send reset password");
+    }
+
+    // try {
+    //   // Check if the email exists in Firebase authentication
+    //   const methods = await fetchSignInMethodsForEmail(auth, email);
+    //   console.log(methods);
+    //   if (methods && methods.length > 0) {
+    //     // Email exists, now check if the user is verified
+    //     const user = auth.currentUser;
+        
+    //     if (user && user.emailVerified) {
+    //       // User is verified, proceed with sending password reset email
+    //       await sendPasswordResetEmail(auth, email);
+    //       toast.success("Password reset email was sent");
+    //     } else {
+    //       // User is not verified, display an error message
+    //       toast.error("Please verify your email before resetting your password");
+    //     }
+    //   } else {
+    //     // Email does not exist in authentication
+    //     toast.error("Email does not exist in our database");
+    //   }
+    // } catch (error) {
+    //   toast.error("Could not send reset password");
+    // }
+
+
+
+    // try {
+    //   // Check if the email exists in Firebase authentication
+    //   console.log('email current: ', email);
+    //   const methods = await fetchSignInMethodsForEmail(auth, email);
+    //   console.log('email: ', methods, methods.length);
+    //   if (methods && methods.length > 0) {
+    //     // Email exists, proceed with sending password reset email
+
+
+    //     await sendPasswordResetEmail(auth, email);
+    //     toast.success("Password reset email was sent");
+    //   } else {
+    //     // Email does not exist in authentication
+    //     toast.error("Email does not exist in our database");
+    //   }
+    // } catch (error) {
+    //   toast.error("Could not send reset password");
+    // }
+
+
+
+    // try {
+    //   await sendPasswordResetEmail(auth, email)
+    //   setEmailMessage(true);
+    //   alert('sent')
+    // } catch (error) {    
+    //   if (error.code === 'auth/user-not-found') {
+    //     alert('User not found, try again!')
+    //     setEmail('')
+    //     console.log(error, 'User not found')
+    //   }
+    // }
+
+
+  }
+
+  // const onSubmit = async (e) => {
+  //   e.preventDefault();
+  //   const auth = getAuth();
+  //   try {
+
+  //     await sendPasswordResetEmail(auth, email)
+      
+  //     toast.success("Password reset email was sent");
+  //   } catch (error) {    
+  //     if (error.code === 'auth/user-not-found') {
+  //       alert('User not found, try again!')
+  //       setEmail('')
+  //     }
+  //   }
+  // };
+
+
+  // const auth = getAuth();
+  // const onSubmit = async (email) => {
+  //   // event.preventDefault(); // Prevent default form submission
+  //   try {
+  //     await sendPasswordResetEmail(auth, email);
+  //     // alert("Password reset link sent!");
+  //     toast.success("Password reset email was sent");
+  //   } catch (err) {
+  //     console.error(err);
+  //     // alert(err.message);
+  //     toast.error("Could not send reset password");
+  //   }
+  // };
+
+    // Async function to handle form submission
+    // const auth = getAuth();
+    // const onSubmit = async (e) => {
+    //   e.preventDefault(); // Prevent default form submission
+    //   const formData = new FormData(e.target);
+    // const currentEmail = formData.get('email'); // Get email from form data
+    //   console.log(currentEmail);
+    //   try {
+    //     await sendPasswordResetEmail(auth, currentEmail); // Access email from state
+    //     toast.success('Password reset email was sent');
+    //   } catch (err) {
+    //     console.error(err);
+    //     toast.error('Could not send reset password');
+    //   }
+    // };
+
   return (
     <section>
       <h1 className='text-3xl text-center m-t-6 font-bold'>Forgot Password</h1>
@@ -15,8 +140,8 @@ export default function ForgotPassword() {
         className='w-full rounded-2xl' />
       </div>
       <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-        <form >
-          <input type="email" id='email' value={email} 
+        <form onSubmit={onSubmit}>
+          <input type="email" id='email' value={email}  name="email"
           onChange={onChange} placeholder='Email address' 
           className='w-full px-4 py-2 tex-xl text-gray-700 bg-white border-gray-300
           rounded transition ease-in-out mb-6'/>
