40e0123a900edc96e92398059c0f48a769852478
Bakeetha <76912190+Bakeetha@users.noreply.github.com>
2024-05-17 17:14:24 +0530

    Create the places for rent and places for sale pages

--- src/App.js
+++ src/App.js
@@ -12,41 +12,43 @@ import EditListing from './pages/EditListing';
 import Listing from './pages/Listing';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Category from './pages/Category';
 function App() {
   return (
     <>
-   <Router>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/profile" element={<PrivateRoute />}>
-       <Route path="/profile" element={<Profile />}  />
-      </Route>
-      <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/sign-up" element={<SignUp />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="/offers" element={<Offers />} />
-      <Route path="/category/:categoryName/:listingId" element={<Listing />} />
-      <Route path="create-listing" element={<PrivateRoute />} >
-        <Route path="/create-listing" element={<CreateListing />} />
-      </Route>
-      <Route path="edit-listing" element={<PrivateRoute />} >
-        <Route path="/edit-listing/:listingId" element={<EditListing />} />
-      </Route>
-    </Routes>
-   </Router>
-   <ToastContainer
-position="bottom-center"
-autoClose={5000}
-hideProgressBar={false}
-newestOnTop={false}
-closeOnClick
-rtl={false}
-pauseOnFocusLoss
-draggable
-pauseOnHover
-theme="dark"
-/>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/category/:categoryName" element={<Category />} />
+          <Route path="/category/:categoryName/:listingId" element={<Listing />} />
+          <Route path="create-listing" element={<PrivateRoute />} >
+            <Route path="/create-listing" element={<CreateListing />} />
+          </Route>
+          <Route path="edit-listing" element={<PrivateRoute />} >
+            <Route path="/edit-listing/:listingId" element={<EditListing />} />
+          </Route>
+        </Routes>
+      </Router>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </>
   );
 }
